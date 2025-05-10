2cdd4488e51a6312f0c8e9b78d74fc0697121eac
aascenc0 <128317989+aascenc0@users.noreply.github.com>
2025-05-09 22:51:59 -0400

    Create web250-index.php

---
+++ php/web250-index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Alexander Ascencio's Arctic Wolf | WEB250 | Home</title>    
+    <link rel="stylesheet" href="styles/default.css" />
+    <script src="https://lint.page/kit/880bd5.js" crossorigin="anonymous"></script>
+</head>
+<body> 
+    <?php include 'components/header.php'; ?>
+    <main>
+        <h2>Home</h2>
+        <p>Welcome to my personal web page! I’m excited to share my journey as a student at CPCC. 
+        Here, you'll find information about my classes, projects, and interests. I encourage you to explore the sections and 
+        learn more about me in my <a href="./introduction.php">Introduction.</a></p>
+        <p>This site showcases what I am currently working on in WEB250.</p>
+        <img src="images/dog_250.gif" alt="white dog" class="cool-dog" />
+    </main>
+    <?php include 'components/footer.php'; ?>
+    <script src="scripts/HTMLInclude.js"></script>
+</body>
+</html>
