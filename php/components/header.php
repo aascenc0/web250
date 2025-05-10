ca3830e771b75c71084e7c97d7de569f4b31bd20
aascenc0 <128317989+aascenc0@users.noreply.github.com>
2025-05-09 22:45:37 -0400

    Create header.php

---
+++ php/components/header.php
@@ -0,0 +1,13 @@
+<header>
+    <div>
+        <h1>Alexander Ascencio's Arctic Wolf | WEB250</h1>
+    </div>
+          <nav>
+                <ul>
+                  <li><a href="web250-index.php">Home</a></li>
+                    <!-- <li><a href="template.html">Template</a></li> -->
+                  <li><a href="introduction.php">Introduction</a></li>
+                  <li><a href="contract.php">Contract</a></li>
+                </ul>
+          </nav>
+  </header>
