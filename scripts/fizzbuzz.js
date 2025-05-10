56dcffbc917318544667e39705a29aed3f2aae61
aascenc0 <128317989+aascenc0@users.noreply.github.com>
2025-05-10 17:17:37 -0400

    Update fizzbuzz.js

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -53,7 +53,7 @@ document.getElementById("generate-button").addEventListener("click", function ()
     const firstName = document.getElementById("first-name").value.trim();
     const middleInitial = document.getElementById("middle-name").value.trim()[0] || '';
     const lastName = document.getElementById("last-name").value.trim();
-    document.getElementById("greeting").textContent = 'Welcome, ' + firstName + ' ' + middleInitial '.' + ' ' + lastName + '!';
+    document.getElementById("greeting").textContent = 'Welcome, ' + firstName + ' ' + middleInitial + '.' + ' ' + lastName + '!';
 });
 
 // for reset button
