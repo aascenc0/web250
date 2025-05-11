c7d5976323ff313bec4995873862de9c661268f2
aascenc0 <128317989+aascenc0@users.noreply.github.com>
2025-05-11 19:54:39 -0400

    Update header.php

--- php/components/header.php
+++ php/components/header.php
@@ -4,10 +4,14 @@
     </div>
           <nav>
                 <ul>
-                  <li><a href="web250-index.php">Home</a></li>
-                    <!-- <li><a href="template.html">Template</a></li> -->
-                  <li><a href="introduction.php">Introduction</a></li>
-                  <li><a href="contract.php">Contract</a></li>
+                  <li><a href="?page=home">Home</a></li>
+                  <li><a href="?page=introduction">Introduction</a></li>
+                  <li><a href="?page=contract">Contract</a></li>
+                  <li><a href="https://aascenc0.github.io/web250/fizzbuzz.html">Fizz Buzz</a></li>
+                  <li><a href="joyofphp/joyphp/src/">Joy Of Php</a></li>
+                  <li><a href="joyofphp/joyphp/src/samsusedcars.html">Sam's Used Cars</a></li>
+                  <li><a href="multipage_sites/superduper_php">Superduper Php</a></li>
+                  <li><a href="multipage_sites/superduper_static">Superduper Static</a></li>
                 </ul>
           </nav>
   </header>
