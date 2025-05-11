2f1e255e6da672c2c5bf0fc31d6c5eb8315c693c
aascenc0 <128317989+aascenc0@users.noreply.github.com>
2025-05-11 19:52:16 -0400

    Update home.php

--- php/contents/home.php
+++ php/contents/home.php
@@ -1,1 +1,6 @@
-test
+<h2>Home</h2>
+                <p>Welcome to my personal web page! I’m excited to share my journey as a student at CPCC. 
+                Here, you'll find information about my classes, projects, and interests. I encourage you to explore the sections and 
+                learn more about me in my <a href="./introduction.html">Introduction.</a></p>
+                <p>This site showcases what I am currently working on in WEB250.</p>
+        <img src="images/dog_250.gif" alt="white dog" class="cool-dog">
