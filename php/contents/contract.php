2c3458456204f06349056717f900b98c69272bd9
aascenc0 <128317989+aascenc0@users.noreply.github.com>
2025-05-11 19:53:46 -0400

    Update contract.php

--- php/contents/contract.php
+++ php/contents/contract.php
@@ -1,1 +1,5 @@
-test
+<h2>Contract</h2>
+        <p>I, <a href="introduction.html">Alexander Ascencio</a>, agree to abide by the terms in my Spring 2025 WEB250-N850 Database Driven Websites course with my instructor, D.I. von Briesen</p>
+        <p>I understand that all work that I do on my school and personal websites will be publicly available to the world. I will not put information there that is inappropriate for schoolwork, or that I wish to keep private.</p>
+        <p>I also understand that it is my work that counts for attendance, not logins or showing up for class. As such, failure to turn in assignments may show as absences.</p>
+        <p>Signed: Alexander Ascencio, 14JAN2025</p>
