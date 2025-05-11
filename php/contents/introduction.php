1f42b72cc02289fd72f11e0ddf834a409d7ad578
aascenc0 <128317989+aascenc0@users.noreply.github.com>
2025-05-11 19:51:37 -0400

    Update introduction.php

--- php/contents/introduction.php
+++ php/contents/introduction.php
@@ -1,1 +1,18 @@
-test
+<main>
+      <h2>Introduction</h2>
+        <img src="images/waterfall.jpeg" alt="standing in front of waterfall" width="488" height="650" />
+        <ul>
+            <li><strong>Personal Background:</strong> I like my privacy. Traveling the world.</li>
+            <li><strong>Academic Background:</strong> Learning.</li>
+            <li><strong>Background in this Subject:</strong> Decking out my Myspace profile back in the day.</li>
+            <li><strong>Primary Computer Platform:</strong> MacOS</li>
+            <li><strong>Courses I'm Taking & Why:</strong>
+                <br>
+                <ul>
+                    <li><strong><a href="contract.php">WEB250</a> - Database Driven Websites:</strong> to learn more about database driven web dev.</li>
+                    <li><strong>CSC221 - Advanced Python Programming:</strong> to learn more about Python</li>
+                    <li><strong>WEB215 - Advanced Markup & Scripting:</strong> to learn more about web dev tools.</li>
+                </ul>
+            </li>
+            <li><strong>Funny/Interesting Item About Yourself:</strong> I’ve traveled a lot around the world, always on the move.</li>
+        </ul>
